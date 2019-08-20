95a71f0ad672b6ab4071865ebee476bd78df653d
Thomas <thomas.bennett3@my.jcu.edu.au>
2019-08-20 10:08:54 +1000

    warm up

---
+++ Lecture_2/Week_2_warm_up.py
@@ -0,0 +1,29 @@
+# SECRET_NUMBER = 6
+# number = int(input("? "))
+# while number != SECRET_NUMBER:
+#    print("no")
+#    number = int(input("? "))
+# print("YAY! Correct")
+
+
+# number_of_people = 0
+# total = 0
+# age = int(input("Age: "))
+# while age >= 0:
+#     total += age
+#     number_of_people += 1
+#     age = int(input("Age: "))
+# # TODO: use Exceptions
+# if number_of_people == 0:
+#     print("Not defined")
+# else:
+#     average = total / number_of_people
+#     print(average)
+
+# name = "Thomas"
+# for i in range(0, 130, 40):
+#     print("{1:8} {0:3}".format(i, name[i:]))
+
+name = "Thomas Bennett"
+for i, character in enumerate(name):
+    print("{:2} = {} ({:3})".format(i, character))
